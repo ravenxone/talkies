fe1913311ef16707d35e21544187d25bd0e9d7e6
ravenxone <divyanshatwork@gmail.com>
2021-04-13 19:13:42 +0530

    css updates

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Jumbotron from './components/jumbotron'
+
+export default function App() {
+  return (
+   <Jumbotron />
+  );
+}
+
+
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {inner} from './styles/Jumbotron'
+
+export default function Jumbotron(){
+ return (
+     <inner>
+         <p>Hello again...</p>
+     </inner>
+ );
+};
+
+
+
